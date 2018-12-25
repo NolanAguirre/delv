ed4b39813557e87e5d0079c2ea04ed686c38a03e
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-12-25 00:22:58 -0600

    rework cache to filter args, start working on react components

--- src/Cache.js
+++ src/Cache.js
@@ -1,134 +1,126 @@
+import graphql from 'graphql-anywhere'
+import typeMap from './TypeMap'
+import CacheEmitter from './CacheEmitter';
 const gql = require('graphql-tag')
 const util = require('util');
-import graphql from 'graphql-anywhere'
 var _ = require('lodash');
-var pluralize = require('pluralize')
-var fs = require('fs');
 
-const blacklistFields = ['node', 'id', 'nodeId', 'nodes', 'edges']
-
-const blacklistTypes = [null, 'Node', 'Int', 'String', 'Cursor', 'UUID', 'Boolean', 'PageInfo', 'Float', 'Mutation', 'ID', 'Datetime', '__Type',  '__Schema', '__Directive', '__EnumValue', '__Field', '__InputValue']
 
 class Cache {
     constructor() {
         this.cache = {};
-        this.fields = new Map();
-        this.queries = {};
     }
 
     resolver = (fieldName, root, args, context, info) => {
-        if(info.isLeaf){
-            if(root.hasOwnProperty(fieldName)){
+        if (info.isLeaf) {
+            if (root.hasOwnProperty(fieldName)) {
                 return root[fieldName];
-            }else{
+            } else {
                 throw new Error('Some of the data requested in the query is not in the cache')
             }
         }
-        if(fieldName === 'nodes'){
+        if (fieldName === 'nodes') {
             return Object.values(root)
         }
-        let fieldType = this.fields.get(fieldName);
-        if(fieldType){
-            if(fieldType.endsWith('Connection')){
-                if(root === null){
-                    let temp = this.cache[this.guessChildType(fieldType)];
-                    if(temp){
-                        return temp;
+        let fieldType = typeMap.get(fieldName);
+        if (fieldType) {
+            if (fieldType.endsWith('Connection')) {
+                fieldType = typeMap.guessChildType(fieldType);
+            }
+            let connections = root[fieldType]
+            if (connections) {
+                let ids
+                if(connections instanceof Object){
+                    if(Array.isArray(connections)){
+                        ids = connections;
                     }else{
-                        throw new Error('Some of the data requested in the query is not in the cache')
+                        ids = Object.keys(connections);
                     }
-                }
-                let connections =  root[fieldType]
-                if(connections){
-                    let childType = Object.keys(connections)[0]
-                    let ids = connections[childType]
-                    return this.filterCacheById(childType, ids)
                 }else{
-                    throw new Error('Some of the data requested in the query is not in the cache')
+                    return this.cache[fieldType][connections]
                 }
+
+                let nextRoot = this.filterCacheById(fieldType, ids)
+                if(args){
+                    return this.filterCache(nextRoot, args)
+                }
+                return nextRoot
+            } else {
+                throw new Error('Some of the data requested in the query is not in the cache')
             }
-            return this.cache[fieldType][root[fieldType]]
         }
-
-        return {};
+        return this.cache[fieldType][root[fieldType]]
     }
 
-    loadIntrospection = (queryResult) => {
-        queryResult['__schema'].types.forEach((type)=>{
-            if(type.fields && !blacklistTypes.includes(type.name) && !type.name.endsWith('Payload')){
-                type.fields.forEach((field)=>{
-                    let typeName = field.type.name;
-                    if(typeName === null){
-                        typeName = field.type.ofType.name
+    filterCache = (set, args) => {
+        if(args.condition){
+            return _.pickBy(set, function(value, key) {
+                let match = true;
+                for(let k in args.condition){
+                    if(value[k] !== args.condition[k]){
+                        match = false;
                     }
-                    if(!blacklistTypes.includes(typeName)){
-                        this.fields.set(field.name, typeName)
-                    }
-                })
-            }
-        })
-    }
-
-    guessParentType = (type) => {
-        return pluralize(type) + "Connection"
-    }
-
-    guessChildType = (type) => {
-        return pluralize.singular(type.slice(0,-10))
-    }
-
-    customizer = (objValue, srcValue, key, object, source, stack) => {
-        if(Array.isArray(objValue)){
-            return _.union(objValue, srcValue);
+                }
+                return match;
+            });
+        }else{
+            return set;
         }
     }
 
     merge = (oldObj, newObj) => {
-        return _.mergeWith(oldObj, newObj, this.customizer);
+        let customizer = customizer = (objValue, srcValue, key, object, source, stack) => {
+            if (Array.isArray(objValue)) {
+                return _.union(objValue, srcValue);
+            }
+        }
+        return _.mergeWith(oldObj, newObj, customizer);
     }
 
     formatObject = (object, parentType) => {
         let returnVal = {};
-        for(var key in object){
+        for (var key in object) {
             let value = object[key]
-            if(value instanceof Object){
-                const rootType = this.fields.get(key)
-                if(!rootType){
+            if (value instanceof Object) {
+                const rootType = typeMap.get(key)
+                if (!rootType) {
                     throw new Error(`Line 76: A type was not mapped for field ${rootType}`)
                 }
-                if(value.nodes){
-                    let nodes = value.nodes.map((node)=>{
-                        if(node.nodeId){
-                            this.formatObject(node, this.guessChildType(rootType))
+                if (value.nodes) {
+                    let nodes = value.nodes.map((node) => {
+                        if (node.nodeId) {
+                            this.formatObject(node, typeMap.guessChildType(rootType))
                             return node.nodeId;
-                        }else{
+                        } else {
                             throw new Error('Line 57: query object did not have required field nodeId')
                         }
                     });
 
-                    returnVal[rootType] = {
-                        [this.guessChildType(rootType)]:nodes
-                    }
-                }else{
-                    if(value.nodeId){
+                    returnVal[typeMap.guessChildType(rootType)] = nodes
+                } else {
+                    if (value.nodeId) {
                         this.formatObject(value, rootType)
                         returnVal[rootType] = value.nodeId
-                    }else{
+                    } else {
                         throw new Error('Line 64: query object did not have required field nodeId')
                     }
                 }
-            }else{
+            } else {
                 returnVal[key] = object[key]
             }
         }
-        if(parentType){
-            if(!this.cache[parentType]){
+        if (parentType) {
+            if (!this.cache[parentType]) {
                 this.cache[parentType] = {}
             }
             let cacheVal = this.cache[parentType][object.nodeId]
-            if(cacheVal){
-                this.cache[parentType][object.nodeId] = this.merge(cacheVal, returnVal)
-            }else{
+            if (cacheVal) {
+                if (!_.isEqual(cacheVal, returnVal)) {
+                    CacheEmitter.changeType(parentType)
+                    this.cache[parentType][object.nodeId] = this.merge(cacheVal, returnVal)
+                }
+            } else {
+                CacheEmitter.changeType(parentType)
                 this.cache[parentType][object.nodeId] = returnVal;
             }
         }
@@ -143,20 +135,22 @@ class Cache {
     processIntoCache = (queryResult) => {
         let result = _.cloneDeep(queryResult)
         this.formatObject(result)
-        var json = JSON.stringify(this.cache);
-        fs.writeFile('cache.json', json, 'utf8', (error) => console.log(error));
+        CacheEmitter.emitCacheUpdate();
     }
 
     loadQuery = (query) => {
-        try{
-            return {data:graphql(this.resolver, gql`${query}`, null)}
-        } catch(error){
-            if(error.message === 'Some of the data requested in the query is not in the cache'){
-                return {query:true}
+        try {
+            return graphql(this.resolver, gql `${query}`, this.cache)
+        } catch (error) {
+            if (error.message === 'Some of the data requested in the query is not in the cache') {
+                return {
+                    query: true
+                }
             }
-            return error
+            return {}
         }
     }
 }
 
-module.exports = new Cache();
+
+export default new Cache();
--- src/test.js
+++ src/test.js
@@ -3,12 +3,17 @@ const util = require('util');
 const axios = require('axios')
 const cache = require('./Cache')
 import graphql from 'graphql-anywhere'
-import Delv from './j-delv'
+import Delv from './delv'
 var fs = require('fs');
 var _ = require('lodash');
 
-new Delv('http://localhost:3005/graphql');
+Delv.setURL('http://localhost:3005/graphql');
+
+Delv.loadIntrospection();
+
 setTimeout(test, 1000)
+setTimeout(test2, 2000)
+// setTimeout(test3, 3000)
 const query = `{
   allActivityCatagories{
     nodes{
@@ -42,95 +47,133 @@ const query = `{
     }
   }
 }`
-const data = {
-    "allActivityCatagories": {
-        "nodes": [{
-            "name": "Classes",
-            "nodeId": "WyJhY3Rpdml0eV9jYXRhZ29yaWVzIiwiMTkyZTQ0MGMtYTRhZC00YTFmLWIxZDktYmZlMTk2ZDhkOGQyIl0=",
-            "activitiesByType": {
-                "nodes": []
-            }
-        }, {
-            "name": "Summer Camps",
-            "nodeId": "WyJhY3Rpdml0eV9jYXRhZ29yaWVzIiwiYzI2M2RiNjQtODY0NS00MjI3LWEzNzUtYTg4MGVmYmJlMjYyIl0=",
-            "activitiesByType": {
-                "nodes": []
-            }
-        }, {
-            "name": "Labs",
-            "nodeId": "WyJhY3Rpdml0eV9jYXRhZ29yaWVzIiwiZjhkNmFlNTUtNTk1ZC00YWI2LTkwMDgtNzIzM2M1NzUwYTZhIl0=",
-            "activitiesByType": {
-                "nodes": [{
-                    "nodeId": "WyJhY3Rpdml0aWVzIiwiMDZkNzFiYTUtMjQ1MC00Mjk2LTg2YzMtMjhmMjNiN2Q2YjBkIl0=",
-                    "name": "Labs",
-                    "description": "asd",
-                    "eventsByEventType": {
-                        "nodes": [{
-                            "nodeId": "WyJldmVudHMiLCI0ZDcxNjQ5Zi1mYzdjLTQwNzgtYjRkOS04MWNkZGZjMzA0MGYiXQ==",
-                            "id": "4d71649f-fc7c-4078-b4d9-81cddfc3040f",
-                            "dateGroupsByEvent": {
-                                "nodes": [{
-                                    "nodeId": "WyJkYXRlX2dyb3VwcyIsIjkwYWY5MjYwLTNiZTgtNGVlYS1hZjllLTM2YzQzODFhZmIyOCJd",
-                                    "price": 100,
-                                    "openRegistration": "2018-12-12T05:59:38",
-                                    "closeRegistration": "2018-12-23T05:59:38"
-                                }]
-                            }
-                        }]
-                    },
-                    "activityCatagoryByType": {
-                        "nodeId": "WyJhY3Rpdml0eV9jYXRhZ29yaWVzIiwiZjhkNmFlNTUtNTk1ZC00YWI2LTkwMDgtNzIzM2M1NzUwYTZhIl0=",
-                        "name": "Labs"
-                    }
-                }, {
-                    "nodeId": "WyJhY3Rpdml0aWVzIiwiZjQ1MzU5ZmEtYjVjYS00NTU2LTg1YmYtMWI4MDk2MjY2MDYwIl0=",
-                    "name": "New Activityasdas",
-                    "description": "asdasd",
-                    "eventsByEventType": {
-                        "nodes": []
-                    },
-                    "activityCatagoryByType": {
-                        "nodeId": "WyJhY3Rpdml0eV9jYXRhZ29yaWVzIiwiZjhkNmFlNTUtNTk1ZC00YWI2LTkwMDgtNzIzM2M1NzUwYTZhIl0=",
-                        "name": "Labs"
-                    }
-                }]
-            }
-        }, {
-            "name": "Workshops",
-            "nodeId": "WyJhY3Rpdml0eV9jYXRhZ29yaWVzIiwiZmRjZGJhZjEtMGVjMy00MTJjLTliZWItZjdhNjU0MTNhZmRkIl0=",
-            "activitiesByType": {
-                "nodes": []
-            }
-        }]
-    }
-}
-function test() {
-    let json;
-    cache.formatObject(data)
-    const cacheResult = graphql(resolver, gql`${query}`, null)
-    console.log(_.isEqual(data, cacheResult))
-    json = JSON.stringify(cacheResult);
-    fs.writeFile('example.json', json, 'utf8', (error) => console.log(error));
-}
 
-const resolver = (fieldName, root, args, context, info) => {
-    if(fieldName === 'nodes'){
-        return Object.values(root)
+const query2 = `{
+  allActivityCatagories{
+    nodes{
+      id
+      nodeId
     }
-    let fieldType = cache.fields.get(fieldName);
-    if(fieldType){
-        if(fieldType.endsWith('Connection')){
-            if(root === null){
-                return cache.cache[cache.guessChildType(fieldType)];
+  }
+}`
+
+const query3 = `{
+  allActivityCatagories{
+    nodes{
+      name
+      id
+      nodeId
+      description
+      activitiesByType{
+        nodes{
+          nodeId
+          name
+          description
+          eventsByEventType{
+            nodes{
+              nodeId
+              id
+              dateGroupsByEvent{
+                nodes{
+                  nodeId
+                  price
+                  openRegistration
+                  closeRegistration
+                }
+              }
             }
-            let connections =  root[fieldType]
-            let childType = Object.keys(connections)[0]
-            let ids = connections[childType]
-            return cache.filterCacheById(childType, ids)
+          }
+          activityCatagoryByType{
+            nodeId
+            name
+          }
         }
-        return cache.cache[fieldType][root[fieldType]]
+      }
     }
-    if(info.isLeaf){
-        return root[fieldName];
-    }
-};
+  }
+}`
+
+
+
+
+function test() {
+    Delv.query(query, {
+        networkPolicy: 'cache-first',
+        onFetch: () => {},
+        resolve: (data) => {
+            const resolver = cache.resolver
+            fs.writeFile('query.json', JSON.stringify(data), 'utf8', (error) => {
+                if (error) {
+                    console.log(error)
+                }
+            });
+            //const cacheResult = graphql(resolver, gql `${query}`, cache.cache)
+            // console.log(_.isEqual(data, cacheResult))
+            // fs.writeFile('example.json', JSON.stringify(cacheResult), 'utf8', (error) => {
+            //     if (error) {
+            //         console.log(error)
+            //     }
+            // });
+        }
+    })
+}
+function test2() {
+    Delv.query(query, {
+        networkPolicy: 'cache-first',
+        onFetch: () => {},
+        resolve: (data) => {
+            fs.writeFile('example.json', JSON.stringify(data), 'utf8', (error) => {
+                if (error) {
+                    console.log(error)
+                }
+            });
+            //console.log(_.isEqual(data, cacheResult)
+        }
+    })
+}
+//
+// function test2() {
+//     delv.query(
+//         query3, {
+//             networkPolicy: 'cache-first',
+//             resolve: (data) => {
+//                 const resolver = cache.resolver
+//                 fs.writeFile('query.json', JSON.stringify(data), 'utf8', (error) => {
+//                     if (error) {
+//                         console.log(error)
+//                     }
+//                 });
+//                 const cacheResult = graphql(resolver, gql `${query3}`, null)
+//                 console.log(_.isEqual(data, cacheResult))
+//                 fs.writeFile('example.json', JSON.stringify(cacheResult), 'utf8', (error) => {
+//                     if (error) {
+//                         console.log(error)
+//                     }
+//                 });
+//             }
+//         })
+// }
+//
+// function test3() {
+//     delv.query(
+//         query2,
+//         {
+//             networkPolicy: 'cache-first',
+//             resolve: (data) => {
+//                 const resolver = cache.resolver
+//                 fs.writeFile('query.json', JSON.stringify(data), 'utf8', (error) => {
+//                     if (error) {
+//                         console.log(error)
+//                     }
+//                 });
+//                 const cacheResult = graphql(resolver, gql `${query2}`, null)
+//                 console.log(_.isEqual(data, cacheResult))
+//                 fs.writeFile('example.json', JSON.stringify(cacheResult), 'utf8', (error) => {
+//                     if (error) {
+//                         console.log(error)
+//                     }
+//                 });
+//             }
+//         }
+//     )
+// }
